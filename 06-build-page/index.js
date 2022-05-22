5b07531c7e66abfbedb6c0d40e524d24e0c6fd2e
Vitaliy Dreko <vitali.dreko@gmail.com>
2022-05-22 03:21:30 +0300

    feat: add bundle styles from styles folder

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,11 +5,24 @@ const fsProm = require('fs/promises');
 const projectDistName = 'project-dist';
 const projectDistPath = path.join(__dirname, projectDistName);
 
+const originalStylesPath = path.join(__dirname, 'styles');
+const bundlePath = path.join(projectDistPath, 'styles.css');
 
+async function mergeStyles() {
+  const write = fs.createWriteStream(bundlePath, 'utf-8');
+  const files = await fsProm.readdir(originalStylesPath, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isFile() && path.extname(file.name) === '.css') {
+      const read = fs.createReadStream(path.join(originalStylesPath, file.name), 'utf-8');
+      read.pipe(write);
+    }
+  }
+}
 
 async function buildPage() {
   await fsProm.rm(projectDistPath, {force: true, recursive: true});
   await fsProm.mkdir(projectDistPath, {recursive: true});
+  await mergeStyles();
 }
 
 buildPage();
